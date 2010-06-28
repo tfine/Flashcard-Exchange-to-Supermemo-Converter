3aeca4e31021276442bc929bf2133500b34dc2ea
Todd Fine <tdfine@gmail.com>
2010-06-27 23:56:21 -0400

    add simple parser for reverse

--- flashsupermemo.py
+++ flashsupermemo.py
@@ -1,9 +1,19 @@
+#converter for CSV Flashcard Exchange Format to Supermemo Q&A
+#entered into public domain by Todd Fine
+
 import csv
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("--r", "--reverse",
+                  action="store_true", dest="reverse", default=False,
+                  help="reverse questions and answers")
+(options, args) = parser.parse_args()
 
 filename = "pol.csv"
 readera = csv.reader(open(filename, "r"))
 
-e = open("bird1.txt", "w")
+e = open("default.txt", "w")
 
 for x in readera:
     e.write("q: ")
@@ -13,11 +23,12 @@ for x in readera:
     e.write(x[1])
     e.write("\n")
     e.write("\n")
-    e.write("q: ")
-    e.write(x[1])
-    e.write("\n")
-    e.write("a: ")
-    e.write(x[0])
-    e.write("\n")
-    e.write("\n")
+    if options.reverse:
+        e.write("q: ")
+        e.write(x[1])
+        e.write("\n")
+        e.write("a: ")
+        e.write(x[0])
+        e.write("\n")
+        e.write("\n")
 e.close()
